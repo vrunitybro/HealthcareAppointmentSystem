645950bef144f79a00480d2a2a316b0e8e79ffa7
vrunitybro <vrunitybro@gmail.com>
2026-09-05 10:02:58 -0600

    feat: add appointment request validation

--- DTOs/CreateAppointmentDto.cs
+++ DTOs/CreateAppointmentDto.cs
@@ -1,12 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace HealthcareAppointmentSystem.DTOs;
 
 public class CreateAppointmentDto
 {
+    [Range(1, int.MaxValue)]
     public int PatientId { get; set; }
 
+    [Range(1, int.MaxValue)]
     public int ProviderId { get; set; }
 
+    [Required]
     public DateTime AppointmentDate { get; set; }
 
+    [Required]
+    [StringLength(500, MinimumLength = 3)]
     public string Reason { get; set; } = string.Empty;
 }
